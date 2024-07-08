7cc44e803adb1599e1f121b59ad57f3fa88780fe
minagerges <minagerges651@gmail.com>
2024-07-08 19:58:23 +0300

    add creatProduct Hadler

--- src/Services/Catalog/Catalog.API/Products/CreatProduct/CreatProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreatProduct/CreatProductHandler.cs
@@ -1,14 +1,26 @@
-﻿using MediatR;
+﻿using BuildingBlock.CQRS;
+using Catalog.API.Models;
+using MediatR;
 
 namespace Catalog.API.Products.CreatProduct
 {
-    public record CreatProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): IRequest<CreatProductResult>;
+    public record CreatProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): ICommand<CreatProductResult>;
     public record CreatProductResult(Guid Id);
-    public class CreatProductCommandHandler : IRequestHandler<CreatProductCommand, CreatProductResult>
+    public  class CreatProductCommandHandler : ICommandHandler<CreatProductCommand, CreatProductResult>
     {
-        public Task<CreatProductResult> Handle(CreatProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreatProductResult> Handle(CreatProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var product = new Product()
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+
+            };
+            return new CreatProductResult(Guid.NewGuid());
+
         }
     }
 }
--- src/Services/Catalog/Catalog.API/Products/CreatProduct/CreatProuctEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreatProduct/CreatProuctEndPoint.cs
@@ -1,7 +1,27 @@
-﻿namespace Catalog.API.Products.CreatProduct
+﻿
+namespace Catalog.API.Products.CreatProduct
 {
- 
-    public class CreatProuctEndPoint
+
+    public record CreatProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreatProductRespnse(Guid Id);
+    public class CreatProuctEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/Products", async (CreatProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreatProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreatProductRespnse>();
+                return Results.Created($"/Products/{response.Id}", response);
+
+            })
+            .WithName("CreatProduct")
+            .Produces<CreatProductRespnse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Creat Product")
+            .WithDescription("Crat Product");
+        }
     }
+
 }
